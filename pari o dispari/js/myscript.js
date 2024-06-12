7b66b1a479cab0224fded00c1ce536190e60e5f1
flavio-franceschetti <flaviofranceschetti98@gmail.com>
2024-06-12 18:01:45 +0200

    feat [even or odd]: add user prompt date

---
+++ pari o dispari/js/myscript.js
@@ -0,0 +1,9 @@
+// L’utente sceglie pari o dispari
+let userEvenOdd = prompt("Inserisci 'pari' o 'dispari'");
+console.log(userEvenOdd);
+// e inserisce un numero da 1 a 5.
+let userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
+console.log(userNumber);
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
