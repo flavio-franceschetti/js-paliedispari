e5db6209144bab5a3003ed079e2f8a3deaed9806
flavio-franceschetti <flaviofranceschetti98@gmail.com>
2024-06-12 17:15:15 +0200

    feat: add return true or false at function, create variable whith function result anda console log a message based on the result

--- js/function.js
+++ js/function.js
@@ -1,17 +1,10 @@
 // creiamo una funzione per testare se la parola inserita è palindroma o meno
-function checkPalindrom(parola) {
+function checkPalindrom(userWord) {
   // trasformiamo la parola inserita in un array per poi girarla riconvertirla in una stringa
   //con .split("") la trasformo in un array e aggiungendo le virgolette divido ogni lettera
   //con .reverse() inverto tutti gli elementi dell'array quindi le lettere
   //con .join("") ritrasformo l'array in una stringa e con le virgolette la stringa verrà tutta attaccata altrimenti viene divisa
-  let parolaReverse = parola.split("").reverse().join("");
-  console.log(parolaReverse);
-  // cotrolliamo se la parola girata è uguale alla parola inserita
-  // se si stampiamo la conferma che la parola è palindroma
-  // altrimenti il contrario
+  let userwordReverse = userWord.split("").reverse().join("");
+  // controllo se la parola invertita è uguale a quella inserita la funzione restituirà true sennò restituirà false
+  return userWord === userwordReverse ? true : false;
 }
-checkPalindrom();
-
-// let original = "Hello World";
-// let reversed = original.split("").reverse().join("");
-// console.log(reversed);
--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,11 @@
-//chiediamo all'utente tramite il prompt una parola per controllare se è palindroma o no
-// const userWord = prompt("Inserisci una parola e ti dirò se è palindroma o no");
-// console.log(userWord);
+// chiediamo all'utente tramite il prompt una parola per controllare se è palindroma o no
+const userWord = prompt("Inserisci una parola e ti dirò se è palindroma o no");
+console.log(userWord);
+
+//creo una variabile con il risultato della funzione true o false
+let isPalindrom = checkPalindrom(userWord);
+console.log("Risultato della funzione:", isPalindrom);
+//creo la condizione dove se true stampo un messaggio di successo altrimenti stamo un messaggio di fallimento
+isPalindrom === true
+  ? console.log("La tua parola è palindroma!!")
+  : console.log("La tua parola NON è palindroma!!!");
