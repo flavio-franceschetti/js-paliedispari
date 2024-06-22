31d40d6f95c39c3481983fc06bd39be81a7f8d7b
flavio-franceschetti <flaviofranceschetti98@gmail.com>
2024-06-22 20:34:18 +0200

    feat: add input and output into the html

--- Parola palindroma/js/myscript.js
+++ Parola palindroma/js/myscript.js
@@ -1,11 +1,19 @@
-// chiediamo all'utente tramite il prompt una parola per controllare se è palindroma o no
-const userWord = prompt("Inserisci una parola e ti dirò se è palindroma o no");
-console.log(userWord);
+// reucpero l'input per l'inserimento della parola
+const userWord = document.getElementById("user-word");
+// recuper il tasto per avviare il check della parola
+const checkBtn = document.getElementById("submit");
+// recupero il div dove inserire il risultato in pagina
+const result = document.getElementById("risultato");
 
-//creo una variabile con il risultato della funzione true o false
-let isPalindrom = checkPalindrom(userWord);
-console.log("Risultato della funzione:", isPalindrom);
-//creo la condizione dove se true stampo un messaggio di successo altrimenti stamo un messaggio di fallimento
-isPalindrom === true
-  ? console.log("La tua parola è palindroma!!")
-  : console.log("La tua parola NON è palindroma!!!");
+// aggiungo l'evento al tasto submit per il check della parola
+checkBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+  let userWordValue = userWord.value;
+  //creo una variabile con il risultato della funzione true o false
+  let isPalindrom = checkPalindrom(userWordValue);
+  console.log("Risultato della funzione:", isPalindrom);
+  //creo la condizione dove se true stampo un messaggio di successo altrimenti stamo un messaggio di fallimento
+  isPalindrom === true
+    ? (result.innerText = "La tua parola è palindroma!!")
+    : (result.innerText = "La tua parola NON è palindroma!!!");
+});
